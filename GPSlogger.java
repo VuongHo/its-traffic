d1be06842a814b07bdafce35f4972351ff848d59
VuongHo <vuongminho68@gmail.com>
2013-10-11 23:19:45 +0700

    remove *.class file, split the methods of GPSLogger to separate file

--- GPSlogger.java
+++ GPSlogger.java
@@ -2,11 +2,9 @@
 * The purpose of method is used to save the raw data, and automatic data processing
 */
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.io.*;
 import java.net.*;
-import com.mongodb.*;
 
 public class GPSlogger {
 	// private static String LOCAL_BROADCAST_ADDRESS = "203.162.44.52";
@@ -82,51 +80,6 @@ public class GPSlogger {
 			}
 			catch (Exception e) {
 				e.printStackTrace();
-			}
-		}
-	}
-
-	public class ReceiveRawData{
-
-		public ReceiveRawData() {
-			
-		}
-
-		public void insertRawGpsData(String[] data){
-			if(data.length < 6) return ;
-			DB db;
-			String date="";
-			String time="";
-			int hour, minute, frameTemp;
-			SimpleDateFormat dateformat = new SimpleDateFormat ("yyyy-MM-dd");
-			SimpleDateFormat timeformat = new SimpleDateFormat ("HH:mm:ss");
-			
-			try {
-				//Real Time
-				Date today = new Date();
-				hour = today.getHours();
-				minute = today.getMinutes();
-				frameTemp = hour*4 + (int)(minute/15);
-				
-				date=dateformat.format(today);
-				time=timeformat.format(today);
-				System.out.println("Date is " +date+" at "+ time);
-
-				MongoClient mongoClient = new MongoClient("localhost", 27017 );
-				db = mongoClient.getDB("hcm_traffic");
-				DBCollection gpsData = db.getCollection("GPSdata");
-				BasicDBObject doc = new BasicDBObject("device_id", data[0]).
-																	append("latitude", Double.parseDouble(data[1])).
-																	append("longitude", Double.parseDouble(data[2])).
-																	append("speed", Double.parseDouble(data[3])).
-																	append("reliability", Double.parseDouble(data[4])).
-																	append("number_of_satellites", Double.parseDouble(data[5])).
-																	append("current_time", new BasicDBObject("date", date).append("frame", frameTemp).append("time", time));
-		    gpsData.insert(doc);
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (Exception e) {
-				System.err.println("Some thing went wrong with mongo!");
 			}
 		}
 	}
---
+++ ReceiveRawData.java
@@ -0,0 +1,48 @@
+import com.mongodb.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.net.UnknownHostException;
+
+public class ReceiveRawData{
+	public ReceiveRawData() {
+		
+	}
+
+	public void insertRawGpsData(String[] data){
+		if(data.length < 6) return ;
+		DB db;
+		String date="";
+		String time="";
+		int hour, minute, frameTemp;
+		SimpleDateFormat dateformat = new SimpleDateFormat ("yyyy-MM-dd");
+		SimpleDateFormat timeformat = new SimpleDateFormat ("HH:mm:ss");
+		
+		try {
+			//Real Time
+			Date today = new Date();
+			hour = today.getHours();
+			minute = today.getMinutes();
+			frameTemp = hour*4 + (int)(minute/15);
+			
+			date=dateformat.format(today);
+			time=timeformat.format(today);
+			System.out.println("Date is " +date+" at "+ time);
+
+			MongoClient mongoClient = new MongoClient("localhost", 27017 );
+			db = mongoClient.getDB("hcm_traffic");
+			DBCollection gpsData = db.getCollection("GPSdata");
+			BasicDBObject doc = new BasicDBObject("device_id", data[0]).
+																append("latitude", Double.parseDouble(data[1])).
+																append("longitude", Double.parseDouble(data[2])).
+																append("speed", Double.parseDouble(data[3])).
+																append("reliability", Double.parseDouble(data[4])).
+																append("number_of_satellites", Double.parseDouble(data[5])).
+																append("current_time", new BasicDBObject("date", date).append("frame", frameTemp).append("time", time));
+	    gpsData.insert(doc);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Some thing went wrong with mongo!");
+		}
+	}
+}
